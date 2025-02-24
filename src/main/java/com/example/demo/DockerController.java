2fac555da76520d8d9c5d8cf8bceb37416ea4809
NamTaekHo <taekho1225@gmail.com>
2025-02-24 14:10:58 +0900

    actions ci/cd  test

--- src/main/java/com/example/demo/DockerController.java
+++ src/main/java/com/example/demo/DockerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DockerController {
     @GetMapping("/test/hello")
     public String hello(){
-        return "Hello World";
+        return "Hello World!!!!!!!!!";
     }
 
     @GetMapping("/test/member/{member-id}")
